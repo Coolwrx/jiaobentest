043e256012dcd43cd663ec1bf5a8a52f561f1d87
Coolwrx <44423508+Coolwrx@users.noreply.github.com>
2019-03-19 23:29:20 +0800

    Update if.sh

--- if.sh
+++ if.sh
@@ -3,3 +3,7 @@
 apt-get update
 apt-get install python-pip
 apt-get install git
+git clone -b manyuser https://github.com/shadowsocksr-backup/shadowsocksr.git
+cd ~/shadowsocksr
+bash initcfg.sh
+python mujson_mgr.py -e -u aaa -p 28593 -k abcd1234 -m aes-128-ctr -O auth_aes128_md5 -o plain
